8ad38f8d80f811a80c9db80b42cf38ca1376a008
jonmlas <jonmlas@gmail.com>
2025-08-21 19:02:53 +0800

    Manifest updated

--- manifest.php
+++ manifest.php
@@ -1,18 +1,36 @@
 <?php if (!defined('FW')) die('Forbidden');
 
-$manifest = array(
-	'version'       => '1.3.32',
-	'display'       => false,
-	'standalone'    => true,
-	'requirements'  => array(
-		'extensions'  => array(
-			'builder' => array(),
-		),
-	),
-	'github_update' => 'ThemeFuse/Unyson-Shortcodes-Extension'
+$manifest = array();
+
+$manifest['name']        = __( 'Shortcodes', 'fw' );
+$manifest['slug']        = 'unysonplus-shortcodes';
+$manifest['description'] = __( 
+	'This extension adds a powerful drag & drop shortcode system. Use it to insert styled content elements anywhere on your site.', 
+	'fw' 
 );
 
-$manifest['github_repo'] = 'https://github.com/ThemeFuse/Unyson-Shortcodes-Extension';
-$manifest['uri'] = 'http://manual.unyson.io/en/latest/extension/shortcodes/index.html#content';
-$manifest['author'] = 'ThemeFuse';
-$manifest['author_uri'] = 'http://themefuse.com/';
+$manifest['version']     = '1.3.32';
+$manifest['display']     = false;
+$manifest['standalone']  = true;
+
+// Requirements
+$manifest['requirements'] = array(
+	'extensions' => array(
+		'builder' => array(),
+	),
+);
+
+// Repository Info
+$manifest['github_update'] = 'UnysonPlus/UnysonPlus-Shortcodes-Extension';
+$manifest['github_repo']   = 'https://github.com/UnysonPlus/UnysonPlus-Shortcodes-Extension';
+$manifest['github_branch'] = 'master';
+
+// Author Info
+$manifest['author']     = 'UnysonPlus';
+$manifest['author_uri'] = 'https://www.lastimosa.com.ph/unysonplus';
+
+// Meta
+$manifest['license']      = 'GPL-2.0-or-later';
+$manifest['text_domain']  = 'fw';
+$manifest['requires_php'] = '7.4';
+$manifest['requires_wp']  = '5.8';
