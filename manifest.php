84e4243aebb795ca951eb99842aa0ffe73bd218b
jonmlas <jonmlas@gmail.com>
2026-05-20 20:16:32 +0800

    Added text-expander Shortcodes

--- manifest.php
+++ manifest.php
@@ -9,7 +9,7 @@ $manifest['description'] = __(
 	'fw' 
 );
 
-$manifest['version']     = '1.4.0';
+$manifest['version']     = '1.4.1';
 $manifest['display']     = false;
 $manifest['standalone']  = true;
 
